11222ad36775f07f58f8b34efcf4a5846e7c0d67
Sachin-kr21 <sachin210302@gmail.com>
2024-03-17 23:43:12 +0530

    merge conflict correction

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const app = require("./app");
 
 app.listen(8101, () => {
-<<<<<<< HEAD
   console.log("Started express server at port 8101");
-=======
-  console.log("Started express server at port 8100");
->>>>>>> eb5621dfaa901a4a988a5b7bdb0c71801db57666
+
+
 });
